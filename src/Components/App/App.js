34c3411f674d41ee55540d67ba7b596660fa9bce
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-27 18:10:35 -0700

    Add NavBar component to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.scss';
 import SVGPage from '../SVGPage/SVGPage';
+import NavBar from '../NavBar/NavBar';
 
 class App extends Component {
   
   render() {
     return (
       <main>
-         <SVGPage />
+        <NavBar />
+        <SVGPage />
 
       </main>
     )
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.scss';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Consuelo Sierra</h1>
+            <button>About</button>
+            <button>Projects</button>
+            <button>Contact</button>
+        </nav>
+    )
+}
+
+export default NavBar;
