d93951fccc4edc9ee5ffb92dc7a9b16977bba853
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-27 18:31:46 -0700

    Add new about folder and route it in App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.scss';
 import SVGPage from '../SVGPage/SVGPage';
+// import ProjectsContainer from '../ProjectsContainer';
+// import Contact from '../Contact/Contact';
+// import About from '../About/About';
 import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
@@ -10,6 +13,9 @@ class App extends Component {
       <main>
         <Switch>
           <Route exact path='/' render={() => <SVGPage /> } />
+          {/* <Route exact path='/about' render={() => <About /> } /> */}
+          {/* <Route exact path='/projects' render={() => <ProjectsContainer /> } /> */}
+          {/* <Route exact path='/contact' render={() => <Contact /> } /> */}
         </Switch>
 
       </main>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,8 +7,8 @@ const NavBar = () => {
         <nav>
             <h1>Consuelo Sierra</h1>
             <NavLink to='/about' activeClassName='active-nav' >About</NavLink>
-            <NavLink to='/about' activeClassName='active-nav' >Projects</NavLink>
-            <NavLink to='/about' activeClassName='active-nav' >Contact</NavLink>
+            <NavLink to='/projects' activeClassName='active-nav' >Projects</NavLink>
+            <NavLink to='/contact' activeClassName='active-nav' >Contact</NavLink>
         </nav>
     )
 }
