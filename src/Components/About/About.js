5d192aad723bc1470d617a74ba73842a7ce356cc
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-30 18:06:03 -0700

    Add image of me to about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,1 +1,15 @@
-   
+import React from 'react';
+import './About.scss';
+
+const About = () => {
+  return (
+    <section className='about'>
+        <img src='https://media.licdn.com/dms/image/C4D03AQFtPmb2kohgGg/profile-displayphoto-shrink_200_200/0?e=1580947200&v=beta&t=7E50_2FcNCJ7SCKrg9mirhuV29G66GYp_3ig201m5kE' alt='picture of Consuelo' />
+        <section className='paragraph'>
+             <p> Hiya! My name is Consuelo, I am a fronntend developer</p>
+        </section>
+    </section>
+  )
+}
+
+export default About;
