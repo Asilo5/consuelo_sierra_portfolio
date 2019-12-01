82eb24df6c4b5929ab54f920d5ba66a338690f13
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-30 21:14:28 -0700

    Add links and intro

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,9 +4,20 @@ import './About.scss';
 const About = () => {
   return (
     <section className='about'>
-        <img src='https://media.licdn.com/dms/image/C4D03AQFtPmb2kohgGg/profile-displayphoto-shrink_200_200/0?e=1580947200&v=beta&t=7E50_2FcNCJ7SCKrg9mirhuV29G66GYp_3ig201m5kE' alt='picture of Consuelo' />
-        <section className='paragraph'>
-             <p> Hiya! My name is Consuelo, I am a fronntend developer</p>
+        <section>
+            <img src='https://media.licdn.com/dms/image/C4D03AQFtPmb2kohgGg/profile-displayphoto-shrink_200_200/0?e=1580947200&v=beta&t=7E50_2FcNCJ7SCKrg9mirhuV29G66GYp_3ig201m5kE' alt='Consuelo posing outside holding her laptop in Denver Colorado' />
+            <section className='paragraph'>
+                <h2> Hiya! My name is Consuelo.</h2>
+                <p> I am a Front-End Developer based in Denver, Colorado and willing to relocate.</p>
+                <p> I have a passion for creating websites that are fun yet easy to use for users using React.</p>
+                <p> Currently, the opportunities I am looking for are junior Front-End, internship, or apprenticeship positions.</p>
+            </section>
+        </section>
+          <img src='https://www.freepngimg.com/download/social_media/74289-media-icons-computer-linkedin-social-free-transparent-image-hd.png' />
+          <img src='https://icons-for-free.com/iconfiles/png/512/logo+twitter+twitter+logo+icon-1320167831451644641.png' />
+          <img src='https://icon-library.net/images/github-icon-png/github-icon-png-22.jpg' />
+        <section>
+    
         </section>
     </section>
   )
