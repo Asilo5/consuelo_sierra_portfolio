204997005764670bfe65026c96099e93ecc2e293
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-30 22:00:44 -0700

    Add style to About component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,17 +7,19 @@ const About = () => {
         <section className='about-chelo'>
             <img src='https://media.licdn.com/dms/image/C4D03AQFtPmb2kohgGg/profile-displayphoto-shrink_200_200/0?e=1580947200&v=beta&t=7E50_2FcNCJ7SCKrg9mirhuV29G66GYp_3ig201m5kE' alt='Consuelo posing outside holding her laptop in Denver Colorado' />
             <section className='paragraph'>
-                <h2> Hiya! My name is Consuelo.</h2>
-                <p> I am a Front-End Developer based in Denver, Colorado and willing to relocate.</p>
-                <p> I have a passion for creating websites that are fun yet easy to use for users using React.</p>
-                <p> Currently, the opportunities I am looking for are junior Front-End, internship, or apprenticeship positions.</p>
+                <h2> Hiya!</h2>
+                <h2>I'm Consuelo.</h2>
+                <article>
+                    <p> I am a Front-End Developer based in Denver, Colorado and willing to relocate.</p>
+                    <p> I have a passion for creating websites that are fun yet easy to use for users using React.</p>
+                    <p> Currently, the opportunities I am looking for are junior Front-End, internship, or apprenticeship positions.</p>
+                </article>
             </section>
         </section>
-          <img className='link' src='https://www.freepngimg.com/download/social_media/74289-media-icons-computer-linkedin-social-free-transparent-image-hd.png' alt='linkedin icon'/>
-          <img className='link' src='https://icons-for-free.com/iconfiles/png/512/logo+twitter+twitter+logo+icon-1320167831451644641.png' alt='twitter icon' />
-          <img className='link' src='https://icon-library.net/images/github-icon-png/github-icon-png-22.jpg' alt='github icon' />
-        <section>
-    
+        <section className='link-section'>
+            <img className='link' src='https://www.freepngimg.com/download/social_media/74289-media-icons-computer-linkedin-social-free-transparent-image-hd.png' alt='linkedin icon'/>
+            <img className='link' src='https://icons-for-free.com/iconfiles/png/512/logo+twitter+twitter+logo+icon-1320167831451644641.png' alt='twitter icon' />
+            <img className='link' src='https://icon-library.net/images/github-icon-png/github-icon-png-22.jpg' alt='github icon' />
         </section>
     </section>
   )
