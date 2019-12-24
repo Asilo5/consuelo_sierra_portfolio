b11ccba245ff724caf1b657b0de4dc434222089c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-24 10:38:15 -0800

    Add gradient background to navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,11 +5,9 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
     return (
         <nav>
-            <section className='svg-links'>
                 <NavLink to='/about' className='nav' ><span className='nav-text'>About Me</span></NavLink>
                 <NavLink to='/projects' className='nav' ><span className='nav-text'>Projects</span></NavLink>
                 <NavLink to='/contact' className='nav' ><span className='nav-text'>Contact</span></NavLink>
-            </section>
         </nav> 
     )
 }
