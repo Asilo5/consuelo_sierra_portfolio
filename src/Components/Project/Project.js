7826078b98eac4c5c70d88a083fd0a71a650b10c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-12-24 18:31:32 -0800

    Add style to projects

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './Project.scss';
 
 const Project = ({ name, description, image, stack }) => {
 
     let techStack = stack.map((tech) => <p key={tech}>{tech}</p>)
     return (
-      <section>
+      <section className='project'>
          <h3>{name}</h3>
          <img src={image} alt={name}/>
-         <p>{description}</p>
-         <p>Stack:</p>
-         <p>{techStack}</p>
+         <section className='project-info'>
+            <p>{description}</p>
+            <p>Stack:</p>
+            <p>{techStack}</p>
+         </section>
       </section>
     )
 }
