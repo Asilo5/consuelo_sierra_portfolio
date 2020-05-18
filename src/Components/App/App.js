bd00c18acc2fb5daa46577775d0da978d2cedc59
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-18 13:48:01 -0600

    Add object of pageTransitions that can cleanly add the opacity state down to motions

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,13 +11,23 @@ import { AnimatePresence } from 'framer-motion';
 class App extends Component {
   
   render() {
+    const pageTransitions = {
+      in: {
+        opacity: 1,
+      },
+
+      out: {
+        opacity: 0
+      }
+    };
+
     return (
       <main>
          <NavBar />
          <AnimatePresence>
-            <Route exact path='/' render={() => <SVGPage /> } />
-            <Route exact path='/projects' render={() => <ProjectsContainer /> }/>
-            {/* <Route exact path='/resume' render={() => <Resume /> }/> */}
+            <Route exact path='/' render={() => <SVGPage pageTransitions={pageTransitions} /> } />
+            <Route exact path='/projects' render={() => <ProjectsContainer pageTransitions={pageTransitions} /> }/>
+            {/* <Route exact path='/resume' render={() => <Resume pageTransitions={pageTransitions} /> }/> */}
          </AnimatePresence>
       </main>
     )
