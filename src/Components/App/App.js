b9440f5873d32be28de0220580109ffb336e9a70
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-18 14:00:52 -0600

    Update class of App to function to better use the useLocation hook for the animation

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,33 +5,37 @@ import SVGPage from '../SVGPage/SVGPage';
 import ProjectsContainer from '../ProjectsContainer/ProjectsContainer';
 import NavBar from '../NavBar/NavBar';
 
-import { Route } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
-class App extends Component {
+const App = () => {
+
+  const location = useLocation();
   
-  render() {
     const pageTransitions = {
       in: {
         opacity: 1,
+        y: 0
       },
 
       out: {
-        opacity: 0
+        opacity: 0,
+        y: "-100%"
       }
     };
 
     return (
       <main>
          <NavBar />
-         <AnimatePresence>
-            <Route exact path='/' render={() => <SVGPage pageTransitions={pageTransitions} /> } />
-            <Route exact path='/projects' render={() => <ProjectsContainer pageTransitions={pageTransitions} /> }/>
-            {/* <Route exact path='/resume' render={() => <Resume pageTransitions={pageTransitions} /> }/> */}
+         <AnimatePresence exitBeforeEnter>
+           <Switch location={location} key={location.pathname} >
+              <Route exact path='/' render={() => <SVGPage pageTransitions={pageTransitions} /> } />
+              <Route exact path='/projects' render={() => <ProjectsContainer pageTransitions={pageTransitions} /> }/>
+              {/* <Route exact path='/resume' render={() => <Resume pageTransitions={pageTransitions} /> }/> */}
+           </Switch>
          </AnimatePresence>
       </main>
     )
   }
-}
 
 export default App;
