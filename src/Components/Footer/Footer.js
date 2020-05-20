113f87c731987eeab63de2d43795b863fa4a5b3b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-19 21:21:00 -0600

    Add name and copy write code for footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,9 +4,13 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <section className='footer'>
-        <a href='https://www.linkedin.com/in/consuelo-sierra-lopez/'> LinkedIn </a>
-        <a href='https://twitter.com/_consueloCodes5'> Twitter </a>
-        <a href='https://github.com/' > GitHub </a>
+        <section className='links'>
+            <a href='https://www.linkedin.com/in/consuelo-sierra-lopez/'> LinkedIn </a>
+            <a href='https://twitter.com/_consueloCodes5'> Twitter </a>
+            <a href='https://github.com/' > GitHub </a>
+        </section>
+
+        <p> &copy; <a href='https://github.com/' > 2020 Consuelo Sierra </a> </p>
     </section>
   );
 };
