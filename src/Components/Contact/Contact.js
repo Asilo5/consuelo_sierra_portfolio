4026979b69f7a4dfbfc8eceee1aa0469c700f57d
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-19 19:55:09 -0600

    Add basic layout for contact component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,1 +1,13 @@
- 
+ import React from 'react';
+ import './Contact.scss';
+
+ const Contact = () => {
+   
+    return (
+        <section className='contact'>
+            
+        </section>
+    )
+ };
+
+ export default Contact;
