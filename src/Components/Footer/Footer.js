b5fdae3fd1635bd571449186ca5d8c2758db0ad0
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-19 21:14:44 -0600

    Add social media links to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,8 +3,10 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <section>
-        
+    <section className='footer'>
+        <a href='https://www.linkedin.com/in/consuelo-sierra-lopez/'> LinkedIn </a>
+        <a href='https://twitter.com/_consueloCodes5'> Twitter </a>
+        <a href='https://github.com/' > GitHub </a>
     </section>
   );
 };
