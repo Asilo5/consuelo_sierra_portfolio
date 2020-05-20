decb9e388d3b6b08d924643d8c6d3f98618efab7
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-19 21:01:26 -0600

    Replace button with link to better use the mailto element

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,5 @@
  import React from 'react';
  import './Contact.scss';
- import { FaEnvelope } from 'react-icons/fa';
 
  const Contact = () => {
    
@@ -9,7 +8,7 @@
             <h2>Looking for new opportunities</h2>
             <p>I'm open to opportunities in the tech field that will challenge me to new positions, languages, and frameworks.</p>
             <p>if you are looking for a new addition in your team or website to be made, contact me below.</p>
-            <button> <FaEnvelope /> Message Me</button>
+            <a href="mailto:asierralopez526@gmail.com"> Message Me </a>
         </section>
     )
  };
