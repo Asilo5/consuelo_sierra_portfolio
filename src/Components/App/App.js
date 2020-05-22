9f591af510d8420bb2d6ea6b81deb29f05079362
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-22 11:43:00 -0600

    Add import of resume to App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,7 @@ import ProjectsContainer from '../ProjectsContainer/ProjectsContainer';
 import NavBar from '../NavBar/NavBar';
 import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
+import Resume from '../Resume/Resume';
 
 import { Route, Switch, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
@@ -37,7 +38,7 @@ const App = () => {
            <Switch location={location} key={location.pathname} >
               <Route exact path='/' render={() => <SVGPage pageVariants={pageVariants} pageTransitions={pageTransitions} /> } />
               <Route exact path='/projects' render={() => <ProjectsContainer pageVariants={pageVariants} pageTransitions={pageTransitions} /> }/>
-              {/* <Route exact path='/resume' render={() => <Resume pageVariants={pageVariants} pageTransitions={pageTransitions}/> }/> */}
+              <Route exact path='/resume' render={() => <Resume pageVariants={pageVariants} pageTransitions={pageTransitions}/> }/>
            </Switch>
          </AnimatePresence>
          <Contact />
