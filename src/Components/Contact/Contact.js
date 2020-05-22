e489fdb7449e6341bde698c7c823962b327c3450
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-22 15:02:36 -0600

    Add new wording to the contact component and fix sintax errors

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,9 +5,9 @@
    
     return (
         <section className='contact'>
-            <h2>Looking for new opportunities</h2>
+            <h2>Contact Me</h2>
             <p>I'm open to opportunities in the tech field that will challenge me to new positions, languages, and frameworks.</p>
-            <p>if you are looking for a new addition in your team or website to be made, contact me below.</p>
+            <p>If you are looking for a new addition in your team or website to be made, contact me below.</p>
             <a href="mailto:asierralopez526@gmail.com"> Message Me </a>
         </section>
     )
