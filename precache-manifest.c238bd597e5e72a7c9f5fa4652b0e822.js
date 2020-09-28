bbdcc2e57644347395d89f5a19851160b8e19ab3
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-09-28 15:13:00 -0700

    Updates

---
+++ precache-manifest.c238bd597e5e72a7c9f5fa4652b0e822.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5206910b7d7215f7dd14cbb0114138",
+    "url": "/le-portfolio/index.html"
+  },
+  {
+    "revision": "e7428b166adadcb170b9",
+    "url": "/le-portfolio/static/css/main.33f5185e.chunk.css"
+  },
+  {
+    "revision": "a6dec1e733e16d298b9b",
+    "url": "/le-portfolio/static/js/2.ce772666.chunk.js"
+  },
+  {
+    "revision": "e7428b166adadcb170b9",
+    "url": "/le-portfolio/static/js/main.964ebf7e.chunk.js"
+  },
+  {
+    "revision": "7d7ad7325dbf78f6cca6",
+    "url": "/le-portfolio/static/js/runtime-main.d180ca17.js"
+  },
+  {
+    "revision": "c8d5f31c25b4f7a6f739231ded07b686",
+    "url": "/le-portfolio/static/media/CV.c8d5f31c.png"
+  },
+  {
+    "revision": "59d199a77681f55fc096d172e4fc10ff",
+    "url": "/le-portfolio/static/media/Consuelo Sierra.59d199a7.pdf"
+  }
+]);
